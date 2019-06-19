29c57ca1b90c411dd728fc92b250abc1c991cdd0
JoaoVSouto <joaovitorss99@hotmail.com>
2019-06-18 21:01:37 -0300

    Sets up layout components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import Navbar from './components/layout/Navbar';
+
+import 'materialize-css/dist/css/materialize.min.css';
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1>MarioPlan</h1>
+        <Navbar />
       </div>
     </BrowserRouter>
   );
